3f69d54cea0474ca7fab288777c63aad67fa8444
Jason Barr <jason@jasonsbarr.com>
2019-12-18 14:43:06 -0600

    Add reducer util functions

---
+++ packages/reducer-utils/index.js
@@ -0,0 +1,4 @@
+import setState from "./src/set-state";
+import makeReducer from "./src/make-reducer";
+
+export {setState, makeReducer};
--- packages/reducer-utils/lib/reducer-utils.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = reducerUtils;
-
-function reducerUtils() {
-    // TODO
-}
