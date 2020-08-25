a2e60348089275ca414d2c1bbd2aae9ba65fe8f6
Jason Barr <me@jasonbarr.dev>
2020-08-25 10:53:37 -0500

    Export useFormState from package

--- packages/custom-hooks/index.js
+++ packages/custom-hooks/index.js
@@ -1,4 +1,5 @@
 import useLocalStorage from "./src/data-storage/use-local-storage";
 import useHttpRequest from "./src/http-requests/use-http-request";
+import useFormState from "./src/form-state/use-form-state";
 
-export { useLocalStorage, useHttpRequest };
+export { useLocalStorage, useHttpRequest, useFormState };
